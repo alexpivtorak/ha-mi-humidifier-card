be6eff31f8b9153a67708aac4e854fa062462fb9
Alex Pivtorak <alex.pivtorak@gmail.com>
2024-12-23 22:37:44 +0100

    chore: update built file for HACS

--- ha-mi-humidifier-card.js
+++ ha-mi-humidifier-card.js
@@ -1,98 +1,4 @@
-var v = Object.defineProperty, x = Object.defineProperties;
-var w = Object.getOwnPropertyDescriptors;
-var m = Object.getOwnPropertySymbols;
-var T = Object.prototype.hasOwnProperty, k = Object.prototype.propertyIsEnumerable;
-var y = (e, t, i) => t in e ? v(e, t, { enumerable: !0, configurable: !0, writable: !0, value: i }) : e[t] = i, f = (e, t) => {
-  for (var i in t || (t = {}))
-    T.call(t, i) && y(e, i, t[i]);
-  if (m)
-    for (var i of m(t))
-      k.call(t, i) && y(e, i, t[i]);
-  return e;
-}, b = (e, t) => x(e, w(t));
-var l = (e, t, i) => new Promise((r, a) => {
-  var o = (s) => {
-    try {
-      u(i.next(s));
-    } catch (h) {
-      a(h);
-    }
-  }, n = (s) => {
-    try {
-      u(i.throw(s));
-    } catch (h) {
-      a(h);
-    }
-  }, u = (s) => s.done ? r(s.value) : Promise.resolve(s.value).then(o, n);
-  u((i = i.apply(e, t)).next());
-});
-import { LitElement as _, css as C, html as g } from "lit";
-import { property as p, customElement as $ } from "lit/decorators.js";
-var H = Object.defineProperty, L = Object.getOwnPropertyDescriptor, d = (e, t, i, r) => {
-  for (var a = r > 1 ? void 0 : r ? L(t, i) : t, o = e.length - 1, n; o >= 0; o--)
-    (n = e[o]) && (a = (r ? n(t, i, a) : n(a)) || a);
-  return r && a && H(t, i, a), a;
-};
-window.customCards = window.customCards || [];
-window.customCards.push({
-  type: "ha-mi-humidifier-card",
-  name: "Mi Humidifier Card",
-  description: "A custom card for Mi Humidifier"
-});
-let c = class extends _ {
-  constructor() {
-    super(...arguments), this.isLoading = !1, this.isTargetLoading = !1, this.pendingTargetHumidity = null, this.debounceTimeout = null, this.targetDebounceTimeout = null;
-  }
-  static getStubConfig() {
-    return {
-      type: "custom:ha-mi-humidifier-card",
-      entity: "humidifier.deerma_jsq5_8f1b_humidifier"
-    };
-  }
-  setConfig(e) {
-    if (!e.entity)
-      throw new Error("Please define an entity");
-    this.config = e;
-  }
-  handlePowerClick() {
-    return l(this, null, function* () {
-      !this.config.entity || this.isLoading || (this.debounceTimeout && clearTimeout(this.debounceTimeout), this.debounceTimeout = setTimeout(() => l(this, null, function* () {
-        try {
-          this.isLoading = !0;
-          const t = this.hass.states[this.config.entity].state === "on" ? "off" : "on", i = t === "on" ? "turn_on" : "turn_off", r = f({}, this.hass.states[this.config.entity]);
-          this.hass.states[this.config.entity] = b(f({}, r), {
-            state: t
-          }), this.requestUpdate(), yield this.hass.callService("humidifier", i, {
-            entity_id: this.config.entity
-          });
-        } catch (e) {
-          console.error("Failed to toggle power:", e);
-        } finally {
-          this.isLoading = !1, this.debounceTimeout = null;
-        }
-      }), 100));
-    });
-  }
-  handleTargetChange(e) {
-    return l(this, null, function* () {
-      !this.config.entity || this.isTargetLoading || (this.targetDebounceTimeout && clearTimeout(this.targetDebounceTimeout), this.targetDebounceTimeout = setTimeout(() => l(this, null, function* () {
-        try {
-          this.isTargetLoading = !0;
-          const t = this.hass.states[this.config.entity], i = this.pendingTargetHumidity !== null ? this.pendingTargetHumidity : t.attributes.target_humidity || 50, r = Math.min(80, Math.max(40, i + e));
-          r !== i && (this.pendingTargetHumidity = r, this.requestUpdate(), yield this.hass.callService("humidifier", "set_humidity", {
-            entity_id: this.config.entity,
-            humidity: r
-          }));
-        } catch (t) {
-          console.error("Failed to change target humidity:", t), this.pendingTargetHumidity = null, this.requestUpdate();
-        } finally {
-          this.isTargetLoading = !1, this.targetDebounceTimeout = null;
-        }
-      }), 100));
-    });
-  }
-  static get styles() {
-    return C`
+var HaMiHumidifierCard=function(r,a,o){"use strict";var v=Object.defineProperty,x=Object.defineProperties;var w=Object.getOwnPropertyDescriptors;var m=Object.getOwnPropertySymbols;var T=Object.prototype.hasOwnProperty,C=Object.prototype.propertyIsEnumerable;var y=(r,a,o)=>a in r?v(r,a,{enumerable:!0,configurable:!0,writable:!0,value:o}):r[a]=o,h=(r,a)=>{for(var o in a||(a={}))T.call(a,o)&&y(r,o,a[o]);if(m)for(var o of m(a))C.call(a,o)&&y(r,o,a[o]);return r},b=(r,a)=>x(r,w(a));var u=(r,a,o)=>new Promise((p,g)=>{var s=t=>{try{e(o.next(t))}catch(n){g(n)}},l=t=>{try{e(o.throw(t))}catch(n){g(n)}},e=t=>t.done?p(t.value):Promise.resolve(t.value).then(s,l);e((o=o.apply(r,a)).next())});var p=Object.defineProperty,g=Object.getOwnPropertyDescriptor,s=(l,e,t,n)=>{for(var i=n>1?void 0:n?g(e,t):e,d=l.length-1,c;d>=0;d--)(c=l[d])&&(i=(n?c(e,t,i):c(i))||i);return n&&i&&p(e,t,i),i};return window.customCards=window.customCards||[],window.customCards.push({type:"ha-mi-humidifier-card",name:"Mi Humidifier Card",description:"A custom card for Mi Humidifier"}),r.MiHumidifierCard=class extends a.LitElement{constructor(){super(...arguments),this.isLoading=!1,this.isTargetLoading=!1,this.pendingTargetHumidity=null,this.debounceTimeout=null,this.targetDebounceTimeout=null}static getStubConfig(){return{type:"custom:ha-mi-humidifier-card",entity:"humidifier.deerma_jsq5_8f1b_humidifier"}}setConfig(e){if(!e.entity)throw new Error("Please define an entity");this.config=e}handlePowerClick(){return u(this,null,function*(){!this.config.entity||this.isLoading||(this.debounceTimeout&&clearTimeout(this.debounceTimeout),this.debounceTimeout=setTimeout(()=>u(this,null,function*(){try{this.isLoading=!0;const t=this.hass.states[this.config.entity].state==="on"?"off":"on",n=t==="on"?"turn_on":"turn_off",i=h({},this.hass.states[this.config.entity]);this.hass.states[this.config.entity]=b(h({},i),{state:t}),this.requestUpdate(),yield this.hass.callService("humidifier",n,{entity_id:this.config.entity})}catch(e){console.error("Failed to toggle power:",e)}finally{this.isLoading=!1,this.debounceTimeout=null}}),100))})}handleTargetChange(e){return u(this,null,function*(){!this.config.entity||this.isTargetLoading||(this.targetDebounceTimeout&&clearTimeout(this.targetDebounceTimeout),this.targetDebounceTimeout=setTimeout(()=>u(this,null,function*(){try{this.isTargetLoading=!0;const t=this.hass.states[this.config.entity],n=this.pendingTargetHumidity!==null?this.pendingTargetHumidity:t.attributes.target_humidity||50,i=Math.min(80,Math.max(40,n+e));i!==n&&(this.pendingTargetHumidity=i,this.requestUpdate(),yield this.hass.callService("humidifier","set_humidity",{entity_id:this.config.entity,humidity:i}))}catch(t){console.error("Failed to change target humidity:",t),this.pendingTargetHumidity=null,this.requestUpdate()}finally{this.isTargetLoading=!1,this.targetDebounceTimeout=null}}),100))})}static get styles(){return a.css`
       ha-card {
         height: 100%;
         background: var(--ha-card-background, var(--card-background-color, #1c1c1c));
@@ -364,62 +270,32 @@ let c = class extends _ {
         color: var(--warning-color, #ff9800);
         margin: 16px 0;
       }
-    `;
-  }
-  handleSliderChange(e) {
-    var i, r;
-    const t = Number(e.target.value);
-    this.config.entity && !this.isTargetLoading && this.handleTargetChange(t - ((r = (i = this.pendingTargetHumidity) != null ? i : this.hass.states[this.config.entity].attributes.target_humidity) != null ? r : 50));
-  }
-  _renderImage() {
-    return g`
+    `}handleSliderChange(e){var n,i;const t=Number(e.target.value);this.config.entity&&!this.isTargetLoading&&this.handleTargetChange(t-((i=(n=this.pendingTargetHumidity)!=null?n:this.hass.states[this.config.entity].attributes.target_humidity)!=null?i:50))}_renderImage(){return a.html`
       <img 
         src="./images/humidifier-1.png"
         alt="Mi Humidifier"
         class="device-image"
-        @error=${(e) => {
-      const t = e.target;
-      if (console.log("❌ Failed to load image. Please check:"), console.log("   /config/www/community/ha-mi-humidifier-card/images/humidifier-1.png"), console.log("Current src:", t.src), t.src.includes("/images/"))
-        console.log("🔄 Trying root path..."), t.src = "./humidifier-1.png";
-      else {
-        t.style.display = "none";
-        const i = t.parentElement;
-        if (i) {
-          const r = document.createElement("div");
-          r.className = "image-error", r.textContent = "⚠️ Image not found", i.appendChild(r);
-        }
-      }
-    }}
+        @error=${e=>{const t=e.target;if(console.log("❌ Failed to load image. Please check:"),console.log("   /config/www/community/ha-mi-humidifier-card/images/humidifier-1.png"),console.log("Current src:",t.src),t.src.includes("/images/"))console.log("🔄 Trying root path..."),t.src="./humidifier-1.png";else{t.style.display="none";const n=t.parentElement;if(n){const i=document.createElement("div");i.className="image-error",i.textContent="⚠️ Image not found",n.appendChild(i)}}}}
       />
-    `;
-  }
-  render() {
-    var a, o, n;
-    if (!this.config || !this.hass || !this.config.entity)
-      return g``;
-    const e = this.hass.states[this.config.entity];
-    if (!e)
-      return g`
+    `}render(){var d,c,f;if(!this.config||!this.hass||!this.config.entity)return a.html``;const e=this.hass.states[this.config.entity];if(!e)return a.html`
         <ha-card>
           <div class="card-content">
             <div class="not-found">Entity not found: ${this.config.entity}</div>
           </div>
         </ha-card>
-      `;
-    const t = e.attributes.current_humidity || 0, i = this.pendingTargetHumidity !== null ? this.pendingTargetHumidity : e.attributes.target_humidity || 50, r = e.state === "on";
-    return g`
+      `;const t=e.attributes.current_humidity||0,n=this.pendingTargetHumidity!==null?this.pendingTargetHumidity:e.attributes.target_humidity||50,i=e.state==="on";return a.html`
       <ha-card>
         <div class="card-content">
           <div class="header">
-            <div class="title">${e.attributes.friendly_name || this.config.entity}</div>
-            <div class="state-text" ?inactive=${!r}>${r ? "ON" : "OFF"}</div>
+            <div class="title">${e.attributes.friendly_name||this.config.entity}</div>
+            <div class="state-text" ?inactive=${!i}>${i?"ON":"OFF"}</div>
           </div>
           
           ${this._renderImage()}
 
           <div class="status">
             <div class="humidity-display">
-              <span class="humidity-value">${i}</span>
+              <span class="humidity-value">${n}</span>
               <span class="humidity-unit">%</span>
             </div>
             <div class="current-humidity">${t}%</div>
@@ -431,81 +307,58 @@ let c = class extends _ {
               min="40"
               max="80"
               step="5"
-              .value=${i}
+              .value=${n}
               class="slider"
               @change=${this.handleSliderChange}
-              ?disabled=${!r}
+              ?disabled=${!i}
             >
           </div>
 
           <div class="controls">
             <button 
               class="control-button"
-              @click=${() => this.handleTargetChange(-5)}
-              ?disabled=${!r}
+              @click=${()=>this.handleTargetChange(-5)}
+              ?disabled=${!i}
             >
               -
             </button>
             <button 
               class="control-button"
-              @click=${() => this.handleTargetChange(5)}
-              ?disabled=${!r}
+              @click=${()=>this.handleTargetChange(5)}
+              ?disabled=${!i}
             >
               +
             </button>
           </div>
 
-          <div class="water-level" data-state=${((a = this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`]) == null ? void 0 : a.state) === "on" ? "empty" : "ok"}>
+          <div class="water-level" data-state=${((d=this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`])==null?void 0:d.state)==="on"?"empty":"ok"}>
             <svg viewBox="0 0 24 24">
               <path fill="currentColor" d="M12,3.77L11.25,4.61C11.25,4.61 9.97,6.06 8.68,7.94C7.39,9.82 6,12.07 6,14.23A6,6 0 0,0 12,20.23A6,6 0 0,0 18,14.23C18,12.07 16.61,9.82 15.32,7.94C14.03,6.06 12.75,4.61 12.75,4.61L12,3.77M12,6.9C12.44,7.42 12.84,7.85 13.68,9.07C14.89,10.83 16,13.07 16,14.23C16,16.45 14.22,18.23 12,18.23C9.78,18.23 8,16.45 8,14.23C8,13.07 9.11,10.83 10.32,9.07C11.16,7.85 11.56,7.42 12,6.9Z" />
             </svg>
             <span class="water-level-text">
-              ${((o = this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`]) == null ? void 0 : o.state) === "on" ? "Tank Empty" : "Tank OK"}
+              ${((c=this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`])==null?void 0:c.state)==="on"?"Tank Empty":"Tank OK"}
             </span>
           </div>
 
-          ${((n = this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`]) == null ? void 0 : n.state) === "on" ? g`
+          ${((f=this.hass.states[`binary_sensor.${this.config.entity.split(".")[1]}_water_shortage_fault`])==null?void 0:f.state)==="on"?a.html`
             <div class="water-warning">
               <svg viewBox="0 0 24 24">
                 <path fill="currentColor" d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" />
               </svg>
               Water tank needs refilling
             </div>
-          ` : ""}
+          `:""}
 
           <button 
             class="power-button"
             @click=${this.handlePowerClick}
-            ?inactive=${!r}
+            ?inactive=${!i}
             ?loading=${this.isLoading}
             ?disabled=${this.isLoading}
           >
-            ${r ? "Turn Off" : "Turn On"}
+            ${i?"Turn Off":"Turn On"}
           </button>
         </div>
       </ha-card>
-    `;
-  }
-};
-d([
-  p({ attribute: !1 })
-], c.prototype, "hass", 2);
-d([
-  p()
-], c.prototype, "config", 2);
-d([
-  p()
-], c.prototype, "isLoading", 2);
-d([
-  p()
-], c.prototype, "isTargetLoading", 2);
-d([
-  p()
-], c.prototype, "pendingTargetHumidity", 2);
-c = d([
-  $("ha-mi-humidifier-card")
-], c);
-export {
-  c as MiHumidifierCard
-};
-//# sourceMappingURL=ha-mi-humidifier-card.js.map
+    `}},s([o.property({attribute:!1})],r.MiHumidifierCard.prototype,"hass",2),s([o.property()],r.MiHumidifierCard.prototype,"config",2),s([o.property()],r.MiHumidifierCard.prototype,"isLoading",2),s([o.property()],r.MiHumidifierCard.prototype,"isTargetLoading",2),s([o.property()],r.MiHumidifierCard.prototype,"pendingTargetHumidity",2),r.MiHumidifierCard=s([o.customElement("ha-mi-humidifier-card")],r.MiHumidifierCard),Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}),r}({},Lit,LitDecorators);
+//# sourceMappingURL=ha-mi-humidifier-card.iife.js.map
