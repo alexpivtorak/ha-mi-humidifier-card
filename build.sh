649ecb06028f23d60985fda4880e40d2981e0b62
Alex Pivtorak <alex.pivtorak@gmail.com>
2024-12-23 03:00:38 +0100

    Test

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@
 npm run build
 
 # Copy the built file to root
-cp dist/ha-mi-humidifier-card.js ./ha-mi-humidifier-card.js 
+cp dist/ha-mi-humidifier-card.js ./ha-mi-humidifier-card.js
+
+# Add and commit the built file
+git add ha-mi-humidifier-card.js
+git commit -m "chore: update built file for HACS"
