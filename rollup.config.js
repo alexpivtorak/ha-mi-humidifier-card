e566b8aae71a6ceaa9f1799f2b0798f766275b9c
Alex Pivtorak <alex.pivtorak@gmail.com>
2024-12-23 17:59:28 +0100

    Build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,25 @@
 import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
-import url from '@rollup/plugin-url';
-
-const dev = process.env.ROLLUP_WATCH;
-
-const serveopts = {
-  contentBase: ['./dist'],
-  host: '0.0.0.0',
-  port: 5000,
-  allowCrossOrigin: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-};
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/ha-mi-humidifier-card.ts',
   output: {
     dir: 'dist',
     format: 'es',
-    entryFileNames: 'ha-mi-humidifier-card.js',
-    assetFileNames: (assetInfo) => {
-      if (assetInfo.name.endsWith('.png')) {
-        return 'humidifier-1.png';
-      }
-      return '[name][extname]';
-    }
   },
   plugins: [
-    url({
-      include: ['**/*.png', '**/*.jpg', '**/*.gif'],
-      limit: 0
-    }),
-    resolve(),
+    nodeResolve(),
+    commonjs(),
     typescript(),
-    dev && serve(serveopts),
-    !dev && terser(),
+    json(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+    terser(),
   ],
 };
