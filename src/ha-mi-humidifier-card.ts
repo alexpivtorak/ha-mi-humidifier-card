45481c93de8e831f02f44d9019fd121968f730a3
Alex Pivtorak <alex.pivtorak@gmail.com>
2025-02-04 20:54:48 +0100

    fix: update imports to use lit-element directly

--- src/ha-mi-humidifier-card.ts
+++ src/ha-mi-humidifier-card.ts
@@ -1,6 +1,6 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { HomeAssistant, fireEvent, LovelaceCard } from 'custom-card-helpers';
+import { LitElement, html, css } from "lit-element";
+import { property, customElement } from "lit-element/decorators.js";
+import { HomeAssistant, LovelaceCard } from "custom-card-helpers";
 
 // This is for typing
 declare global {
